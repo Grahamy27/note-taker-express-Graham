408fbac313d96fa8f9dbdaf8c32efe82241047a2
Zeus <grahamy2711@yahoo.com>
2023-12-18 17:07:01 +0000

    corrected server

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 const express = require("express");
-const api_routes = require('./routes/apiRoutes')
-const html_routes = require('./routes/htmlRoutes')
+const apiRoutes = require('./routes/apiRoutes')
+const htmlRoutes = require('./routes/htmlRoutes')
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-app.use(html_routes)
-app.use(api_routes)
+app.use(htmlRoutes)
+app.use(apiRoutes)
 
-
-require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
